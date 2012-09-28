e63cf33b1cbf8cd62679c129ce13fca5c2cacfc3
Patrick Breheny <patrick.breheny@uky.edu>
2012-09-28 05:49:02 +0000

    version 1.1-0

--- R/setupX.R
+++ R/setupX.R
@@ -1,33 +1,38 @@
 setupX <- function(fit,f,name,nn,cond)
+{
+  ## Set up n x p matrix for (conditional) partial residuals
+  x <- f[,name]
+  x <- if (is.factor(x)) factor(c(1,as.integer(x)),labels=levels(x)) else c(mean(x),x)
+  xdf <- data.frame(x)
+  names(xdf) <- name
+  df <- fillFrame(f,xdf,cond)
+  D <- rbind(f,df)
+  form <- removeFormulaFormatting(formula(fit)[3])
+  
+  if (class(fit)[1]=="mlm") {
+    ind <- apply(is.finite(coef(fit)), 1, all)
+    if (!identical(ind, apply(is.finite(coef(fit)), 1, any))) stop("Inconsistent NA/NaN coefficients across outcomes")
+  } else ind <- is.finite(coef(fit))
+  X. <- model.matrix(as.formula(paste("~",form)),D)[-(1:nrow(f)), ind]
+  X <- t(t(X.[-1,])-X.[1,])
+  
+  ## Set up data frame with nn rows for prediction
+  if(is.factor(x)) xx <- factor(c(1,1:length(levels(x))),labels=levels(x))
+  else xx <- c(mean(x),seq(min(x),max(x),length=nn))
+  xxdf <- data.frame(xx)
+  names(xxdf) <- name
+  df <- fillFrame(f,xxdf,cond)
+  DD <- rbind(f,df)
+  XX. <- NULL
+  XX. <- model.matrix(as.formula(paste("~",formula(fit)[3])),DD)[-(1:nrow(f)), ind]
+  XX <- t(t(XX.[-1,])-XX.[1,])
+  
+  ## Remove extraneous intercept for coxph
+  if (class(fit)[1]=="coxph")
   {
-    ## Set up n x p matrix for (conditional) partial residuals
-    x <- f[,name]
-    if (is.factor(x)) x <- factor(c(1,as.integer(x)),labels=levels(x))
-    else x <- c(mean(x),x)
-    xdf <- data.frame(x)
-    names(xdf) <- name
-    df <- fillFrame(f,xdf,cond)
-    D <- rbind(f,df)
-    form <- removeFormulaFormatting(formula(fit)[3])
-    X. <- model.matrix(as.formula(paste("~",form)),D)[-(1:nrow(f)),is.finite(coef(fit))]
-    X <- t(t(X.[-1,])-X.[1,])
-
-    ## Set up data frame with nn rows for prediction
-    if(is.factor(x)) xx <- factor(c(1,1:length(levels(x))),labels=levels(x))
-    else xx <- c(mean(x),seq(min(x),max(x),length=nn))
-    xxdf <- data.frame(xx)
-    names(xxdf) <- name
-    df <- fillFrame(f,xxdf,cond)
-    DD <- rbind(f,df)
-    XX. <- model.matrix(as.formula(paste("~",formula(fit)[3])),DD)[-(1:nrow(f)),is.finite(coef(fit))]
-    XX <- t(t(XX.[-1,])-XX.[1,])
-
-    ## Remove extraneous intercept for coxph
-    if (class(fit)=="coxph")
-      {
-        XX <- XX[,-which(colnames(XX)=="(Intercept)"),drop=FALSE]
-        X <- X[,-which(colnames(X)=="(Intercept)"),drop=FALSE]
-      }
-
-    return(list(x=x[-1],xx=xx[-1],X=X,XX=XX))
+    XX <- XX[,-which(colnames(XX)=="(Intercept)"),drop=FALSE]
+    X <- X[,-which(colnames(X)=="(Intercept)"),drop=FALSE]
   }
+  
+  return(list(x=x[-1],xx=xx[-1],X=X,XX=XX))
+}
--- R/visreg.R
+++ R/visreg.R
@@ -1,44 +1,44 @@
 ## To do: add overlay plot option?
-visreg <- function(fit, xvar, by, breaks=4, type=c("conditional","effect"), trans=as.numeric, scale=c("linear","response"), xtrans, alpha=.05, nn=101, cond=list(), whitespace=0.2, partial=TRUE, jitter=FALSE, strip.names=FALSE, ...)
-  {
-    if (missing(type) & class(fit)[1]=="coxph") type <- "effect"
-    type <- match.arg(type)
-    scale <- match.arg(scale)
-    if (!missing(by) & !missing(cond)) stop("Cannot specify 'by' and 'cond' simultaneously")
-    if (scale=="response") trans <- family(fit)$linkinv
-
-    f <- setupF(fit)
-    if (missing(xvar)) xvar <- names(f)[-1]
-    for (i in 1:length(xvar)){if (!is.element(xvar[i],names(f))) stop(paste(xvar[i],"not in model"))}
-    if ((!missing(by) & missing(xvar)) | (!missing(by) & (length(xvar) > 1))) stop("Cannot specify 'by' and multiple x variables simultaneously")
-    if (!missing(by) && is.numeric(f[,by]) && length(levels(as.factor(f[,by]))) <= breaks)
-      {
-        attr(f,"needs.update") <- TRUE
-        f[,by] <- as.factor(f[,by])
-        warning("'By' variable has too few unique values and has been coerced to a factor")
-      }
-    if (attr(f,"needs.update")) fit <- update(fit,data=f)
-    cond <- setupCond(cond,f,by,breaks)
-
-    if (length(xvar) > 1 && prod(par("mfcol")) < length(which) && dev.interactive())
-      {
-        oask <- devAskNewPage(TRUE)
-        on.exit(devAskNewPage(oask))
-      }
-
-    if (missing(by))
-      {
-        v <- vector("list",length(xvar))
-        for (i in 1:length(xvar))
-          {
-            v[[i]] <- visregPlot(fit,f,xvar[i],nn,cond[[1]],type,trans,xtrans,alpha,jitter,partial,whitespace,...)
-          }
-        names(v) <- xvar
-        if (length(xvar)==1) v <- v[[1]]
-      }
-    else
-      {
-        v <- visregLatticePlot(fit,f,xvar,nn,cond,type,trans,xtrans,alpha,jitter,partial,whitespace,by,strip.names,...)
-      }
-    return(invisible(v))
+visreg <- function(fit, xvar, by, breaks=4, type=c("conditional","effect"), trans=I, scale=c("linear","response"), xtrans, alpha=.05, nn=101, cond=list(), whitespace=0.2, partial=TRUE, jitter=FALSE, strip.names=FALSE, line.par=NULL, fill.par=NULL, points.par=NULL, ...)
+{
+  if (missing(type) & class(fit)[1]=="coxph") type <- "effect"
+  type <- match.arg(type)
+  scale <- match.arg(scale)
+  if (!missing(by) & !missing(cond)) stop("Cannot specify 'by' and 'cond' simultaneously")
+  if (scale=="response") trans <- family(fit)$linkinv
+  
+  f <- setupF(fit)
+  if (missing(xvar)) xvar <- names(f)[-1]
+  for (i in 1:length(xvar)){if (!is.element(xvar[i],names(f))) stop(paste(xvar[i],"not in model"))}
+  if ((!missing(by) & missing(xvar)) | (!missing(by) & (length(xvar) > 1))) stop("Cannot specify 'by' and multiple x variables simultaneously")
+  if (!missing(by) && is.numeric(f[,by]) && length(levels(as.factor(f[,by]))) <= breaks) {
+    attr(f,"needs.update") <- TRUE
+    f[,by] <- as.factor(f[,by])
+    warning("'By' variable has too few unique values and has been coerced to a factor")
   }
+  if (attr(f,"needs.update")) fit <- update(fit,data=f)
+  cond <- setupCond(cond,f,by,breaks)
+  
+  n.y <- if (class(fit)[1]=="mlm") ncol(coef(fit)) else 1
+  n.plots <- length(xvar) * n.y
+  if (n.plots > 1 && prod(par("mfcol")) < n.plots && dev.interactive() && missing(by)) {
+    oask <- devAskNewPage(TRUE)
+    on.exit(devAskNewPage(oask))
+  }
+  
+  if (missing(by))
+  {
+    v <- vector("list",length(xvar))
+    for (i in 1:length(xvar))
+    {
+      v[[i]] <- visregPlot(fit, f, xvar[i], nn, cond[[1]], type, trans, xtrans, alpha, jitter, partial, whitespace, line.par, fill.par, points.par, ...)
+    }
+    names(v) <- xvar
+    if (length(xvar)==1) v <- v[[1]]
+  }
+  else
+  {
+    v <- visregLatticePlot(fit, f, xvar, nn, cond, type, trans, xtrans, alpha, jitter, partial, whitespace, by, strip.names, line.par, fill.par, points.par, ...)
+  }
+  return(invisible(v))
+}
