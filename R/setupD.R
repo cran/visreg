9fbc459d156bb8833243380c23f5ecb6a2f804b0
Patrick Breheny <patrick.breheny@uky.edu>
2013-05-23 00:00:00 +0000

    version 2.0-0

--- R/setupD.R
+++ R/setupD.R
@@ -1,5 +1,4 @@
-setupD <- function(fit,f,name,nn,cond,whitespace)
-{
+setupD <- function(fit, f, name, nn, cond, whitespace, ...) {
   ## Set up n-row data frame for residuals
   x <- f[,name]
   xdf <- data.frame(x)
@@ -8,15 +7,22 @@ setupD <- function(fit,f,name,nn,cond,whitespace)
   
   form <- removeFormulaFormatting(formula(fit)[3])
   D <- model.frame(as.formula(paste("~",form)),df)
+  condNames <- setdiff(names(D), name)
+  condNames <- intersect(condNames, names(df))
   D <- cbind(D, df[,setdiff(names(df),names(D)), drop=FALSE])
   
   ## Set up nn-row data frame for prediction
-  xx <- if (is.factor(x)) factor(levels(x),levels=levels(x)) else seq(min(x),max(x),length=nn)
+  dots <- list(...)
+  xx <- if (is.factor(x)) {
+    factor(levels(x),levels=levels(x))
+  } else {
+    if ("xlim" %in% names(dots)) seq(dots$xlim[1], dots$xlim[2], length=nn) else seq(min(x),max(x),length=nn)
+  }
   xxdf <- data.frame(xx)
   names(xxdf) <- name
   df <- fillFrame(f,xxdf,cond)
   DD <- model.frame(as.formula(paste("~",form)),df)
   DD <- cbind(DD,df[,setdiff(names(df),names(DD)),drop=FALSE])
   
-  return(list(x=x,xx=xx,D=D,DD=DD))
+  list(x=x, xx=xx, D=D, DD=DD, factor=is.factor(x), name=name, cond=D[1,condNames,drop=FALSE])
 }
--- R/setupF.R
+++ R/setupF.R
@@ -1,21 +1,29 @@
-setupF <- function(fit)
-{
+setupF <- function(fit, xvar) {
   if (class(fit)[1]=="locfit") {
     f <- model.frame(fit)
     for (j in 1:ncol(f)) names(f)[j] <- removeFormulaFormatting(names(f)[j])
   } else {
-    if ("data" %in% names(fit$call)) {
-      f <- as.data.frame(as.list(get_all_vars(fit,eval(fit$call$data,envir=environment(fit$terms)))))
-    } else f <- as.data.frame(as.list(get_all_vars(fit,data=environment(fit$terms))))
+    env <- environment(fit$terms)
+    Data <- eval(fit$call$data, envir=env)
+    f <- as.data.frame(as.list(get_all_vars(fit, Data)))
+    if ("subset" %in% names(fit$call)) {
+      s <- fit$call$subset
+      subset <- eval(substitute(s), Data, env)
+      f <- f[which(subset==TRUE),]
+    } 
   }
   suppressWarnings(f <- f[!apply(is.na(f), 1, any),])
   
   ## Handle some variable type issues
-  attr(f,"needs.update") <- FALSE
-  if (any(sapply(model.frame(fit),class)=="character")) attr(f,"needs.update") <- TRUE
+  needsUpdate <- FALSE
+  if (any(sapply(model.frame(fit),class)=="character")) needsUpdate <- TRUE
   if (any(sapply(f,class)=="logical")) {
-    attr(f,"needs.update") <- TRUE
+    needsUpdate <- TRUE
     for (j in 1:ncol(f)) if (class(f[,j])[1]=="logical") f[,j] <- as.numeric(f[,j])
   }
+  if (missing(xvar)) xvar <- names(f)[-1]
+  for (i in 1:length(xvar)){if (!is.element(xvar[i],names(f))) stop(paste(xvar[i],"not in model"))}
+  attr(f, "needsUpdate") <- needsUpdate
+  attr(f, "xvar") <- xvar
   f
 }
